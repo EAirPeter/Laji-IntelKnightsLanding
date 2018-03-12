60485fef81fbef7960218b271ac938da04dc26c1
EAirPeter <VioletCrestfall@hotmail.com>
2018-03-12 18:00:03 +0900

    misc: add prototypes

---
+++ _prototypes.v
@@ -0,0 +1,132 @@
+`include "cpu.vh"
+
+// Brief: Program Counter, sychronized
+// Description: Update program counter
+module SynPC(clk, rst_n, en, pc_new, pc, pc_4);
+    input clk;
+    input rst_n;
+    input en;
+    input [31:0] pc_new;
+    output [31:0] pc;
+    output [31:0] pc_4;
+endmodule
+
+// Brief: Instruction Memory, synchronized
+// Description: Fetch instruction from memory
+module SynInstMem(clk, rst_n, addr, inst);
+    input clk;
+    input rst_n;
+    input [31:0] addr;
+    output [31:0] inst;
+endmodule
+
+// Brief: Instruction Decoder, combinatorial
+module CmbDecoder(inst, opcode, rs, rt, rd, shamt, funct, imm16, imm26);
+    input [31:0] inst;
+    output [5:0] opcode;
+    output [4:0] rs;
+    output [4:0] rt;
+    output [4:0] rd;
+    output [4:0] shamt;
+    output [5:0] funct;
+    output [15:0] imm16;
+    output [25:0] imm26;
+endmodule
+
+// Brief: Bit Extender, combinatorial
+module CmbExt(imm16, out_sign, out_zero);
+    input [15:0] imm16;
+    output [31:0] out_sign;
+    output [31:0] out_zero;
+endmodule
+
+// Brief: Register File, synchronized
+module SynRegFile(clk, rst_n, req_dbg, req_w, req_a, req_b, data_dbg, data_w, data_a, data_b, data_v0, data_a0);
+    input clk;
+    input rst_n;
+    input en;
+    input [4:0] req_dbg;
+    input [4:0] req_w;
+    input [4:0] req_a;
+    input [4:0] req_b;
+    output [31:0] data_dbg;
+    output [31:0] data_w;
+    output [31:0] data_a;
+    output [31:0] data_b;
+    output [31:0] data_v0;      // For syscall
+    output [31:0] data_a0;      // For syscall
+endmodule
+
+// Brief: Where To Go, combinatorial
+// Description: Compute the value about to be updated into PC
+module CmbWTG(op, off32, imm26, data_x, data_y, pc_4, pc_new, branched);
+    input [`WTG_OP_BIT - 1:0] op;
+    input [31:0] off32;
+    input [25:0] imm26;
+    input [31:0] data_x;
+    input [31:0] data_y;
+    input [31:0] pc_4;
+    output [31:0] pc_new;
+    output branched;        // True on successful conditional branch
+endmodule
+
+// Brief: Arithmetic Logic Unit, combinatorial
+module CmbALU(op, data_x, data_y, shamt, data_res);
+    input [`ALU_OP_BIT - 1:0] op;
+    input [31:0] data_x;
+    input [31:0] data_y;
+    input [4:0] shamt;      // Separate input for shift 
+    output [31:0] data_res;
+endmodule
+
+// Brief: Data Memory, combinatorial
+module SynDataMem(clk, rst_n, en, op, w_en, addr_dbg, addr, data_dbg, data);
+    input clk;
+    input rst_n;
+    input en;
+    input [`DM_OP_BIT - 1:0] op;
+    input w_en;             // Write Enable
+    input [31:0] addr_dbg;  // Address of the data for debugging
+    input [31:0] addr;
+    output [31:0] data_dbg; // Data to be displayed for debugging
+    output [31:0] data;
+endmodule
+
+// Brief: Syscall Module, synchronized
+// Description: If $v0 is 34, display content of $a0 on the 7-segment display;
+//              Otherwise pause the execution of the program
+module SynSyscall(syscall_en, data_v0, data_a0, display, display_en, halt);
+    input syscall_en;       
+    input [31:0] data_v0;
+    input [31:0] data_a0;
+    output [31:0] display;
+    output display_en;
+    output halt;
+endmodule
+
+// Brief: Control Module, synchronized
+module CmbControl(opcode, rt, funct, op_wtg, op_alu, op_datamem, mux_regfile_req_w, mux_regfile_data_w, mux_alu_data_y);
+    input [5:0] opcode;
+    input [4:0] rt;
+    input [5:0] funct;
+    output [`WTG_OP_BIT - 1:0] op_wtg;
+    output [`ALU_OP_BIT - 1:0] op_alu;
+    output [`DM_OP_BIT - 1:0] op_datamem;
+    output [`MUX_RF_REQW_BIT - 1:0] mux_regfile_req_w;
+    output [`MUX_RF_DATAW_BIT - 1:0] mux_regfile_data_w;
+    output [`MUX_ALU_DATAY_BIT - 1:0] mux_alu_data_y;
+endmodule
+
+// Brief: CPU Top Module, synchronized
+module SynLajiIntelKnightsLanding(clk, rst_n, en, regfile_req_dbg, datamem_addr_dbg, regfile_data_dbg, datamem_data_dbg, display, display_en, halt);
+    input clk;
+    input rst_n;
+    input en;
+    input [4:0] regfile_req_dbg;
+    input [31:0] datamem_addr_dbg;
+    output [31:0] regfile_data_dbg;
+    output [31:0] datamem_data_dbg;
+    output [31:0] display;
+    output display_en;
+    output halt;
+endmodule
---
+++ core.vh
@@ -0,0 +1,75 @@
+`ifndef lajiintel_core_vh_
+`define lajiintel_core_vh_
+
+// WhereToGo.operation
+`define WTG_OP_BIT          3
+`define WTG_OP_J32          `WTG_OP_BIT'h0      //  Set pc_new to data_x
+`define WTG_OP_J26          `WTG_OP_BIT'h1      //  Set pc_new to {pc_4[31:28], imm26, 2'b00}
+// Conditional branch, set pc_new to pc_4 + {off32[29:0], 2'b00} when
+//   condition is met. Comparision between data_x and data_y or zero.
+`define WTG_OP_BEQ          `WTG_OP_BIT'h2      
+`define WTG_OP_BNE          `WTG_OP_BIT'h3
+`define WTG_OP_BLEZ         `WTG_OP_BIT'h4
+`define WTG_OP_BGTZ         `WTG_OP_BIT'h5
+`define WTG_OP_BLTZ         `WTG_OP_BIT'h6
+`define WTG_OP_BGEZ         `WTG_OP_BIT'h7
+
+// ArithmeticLogicUnit.operation
+`define ALU_OP_BIT          4
+`define ALU_OP_AND          `ALU_OP_BIT'h0
+`define ALU_OP_OR           `ALU_OP_BIT'h1
+`define ALU_OP_XOR          `ALU_OP_BIT'h2
+`define ALU_OP_NOR          `ALU_OP_BIT'h3
+`define ALU_OP_ADD          `ALU_OP_BIT'h4
+`define ALU_OP_SUB          `ALU_OP_BIT'h5
+`define ALU_OP_SLLV         `ALU_OP_BIT'h6
+`define ALU_OP_SRAV         `ALU_OP_BIT'h7
+`define ALU_OP_SRLV         `ALU_OP_BIT'h8
+// shamt used by the following 3 instructions
+`define ALU_OP_SLL          `ALU_OP_BIT'h9
+`define ALU_OP_SRA          `ALU_OP_BIT'ha
+`define ALU_OP_SRL          `ALU_OP_BIT'hb
+
+`define ALU_OP_SLT          `ALU_OP_BIT'hc
+`define ALU_OP_SLTU         `ALU_OP_BIT'hd
+// data_y << 16, shamt not used
+`define ALU_OP_LUI          `ALU_OP_BIT'he
+
+// DataMemory.operation
+// S stands for Signed, U for Unsigned
+// B for Byte, H for Half-word, WD for word 
+`define DM_OP_BIT           3
+`define DM_OP_SB            `DM_OP_BIT'h0
+`define DM_OP_UB            `DM_OP_BIT'h1
+`define DM_OP_SH            `DM_OP_BIT'h2
+`define DM_OP_UH            `DM_OP_BIT'h3
+`define DM_OP_WD            `DM_OP_BIT'h4
+
+// Multiplexer.RegisterFile.register_number_requested_to_write
+`define MUX_RF_REQW_BIT     2
+// Register $(rd)
+`define MUX_RF_REQW_RD      `MUX_RF_REQW_RD'h0
+// Register $(rt)
+`define MUX_RF_REQW_RT      `MUX_RF_REQW_RD'h1
+// Register $ra
+`define MUX_RF_REQW_31      `MUX_RF_REQW_RD'h2
+
+// Multiplexer.RegisterFile.data_requested_to_write
+`define MUX_RF_DATAW_BIT    2
+// Output of ALU
+`define MUX_RF_DATAW_ALU    `MUX_RF_DATAW_BIT'h0
+// Output of DataMem
+`define MUX_RF_DATAW_DM     `MUX_RF_DATAW_BIT'h1
+// PC + 4
+`define MUX_RF_DATAW_PC4    `MUX_RF_DATAW_BIT'h2
+
+// Multiplexer.ArithmeticLogicUnit.data_y
+`define MUX_ALU_DATAY_BIT   2
+// RegFile port B
+`define MUX_ALU_DATAY_RFB   `MUX_ALU_DATAY_BIT'h0
+// Sign-extended imm16
+`define MUX_ALU_DATAY_EXTS  `MUX_ALU_DATAY_BIT'h1
+// Zero-extended imm16
+`define MUX_ALU_DATAY_EXTZ  `MUX_ALU_DATAY_BIT'h2
+
+`endif
