426f37823891d278fb148efe18905d15e6f8f08d
EAirPeter <VioletCrestfall@hotmail.com>
2018-03-12 18:27:31 +0900

    misc: distribute pots

--- _prototypes.v
+++ _prototypes.v
@@ -1,7 +1,16 @@
-`include "cpu.vh"
+// Pots
+// SynLaji      TopLaji         EAirPeter
+// SynDataMem   CmbExt          cuishaobo
+// CmbControl   CmbDecoder      FluorineDog
+// SynRegFile   SynPC           G-H-Y
+// CmbALU       CmbSyscall      KailinLi
+// CmbWTG       SynInstMem      azure-crab
+
+`include "core.vh"
 
 // Brief: Program Counter, sychronized
 // Description: Update program counter
+// Author: G-H-Y
 module SynPC(clk, rst_n, en, pc_new, pc, pc_4);
     input clk;
     input rst_n;
@@ -13,6 +22,7 @@ endmodule
 
 // Brief: Instruction Memory, synchronized
 // Description: Fetch instruction from memory
+// Author: azure-crab
 module SynInstMem(clk, rst_n, addr, inst);
     input clk;
     input rst_n;
@@ -21,6 +31,7 @@ module SynInstMem(clk, rst_n, addr, inst);
 endmodule
 
 // Brief: Instruction Decoder, combinatorial
+// Author: FluorineDog
 module CmbDecoder(inst, opcode, rs, rt, rd, shamt, funct, imm16, imm26);
     input [31:0] inst;
     output [5:0] opcode;
@@ -34,6 +45,7 @@ module CmbDecoder(inst, opcode, rs, rt, rd, shamt, funct, imm16, imm26);
 endmodule
 
 // Brief: Bit Extender, combinatorial
+// Author: cuishaobo
 module CmbExt(imm16, out_sign, out_zero);
     input [15:0] imm16;
     output [31:0] out_sign;
@@ -41,6 +53,7 @@ module CmbExt(imm16, out_sign, out_zero);
 endmodule
 
 // Brief: Register File, synchronized
+// Author: G-H-Y
 module SynRegFile(clk, rst_n, req_dbg, req_w, req_a, req_b, data_dbg, data_w, data_a, data_b, data_v0, data_a0);
     input clk;
     input rst_n;
@@ -59,6 +72,7 @@ endmodule
 
 // Brief: Where To Go, combinatorial
 // Description: Compute the value about to be updated into PC
+// Author: azure-crab
 module CmbWTG(op, off32, imm26, data_x, data_y, pc_4, pc_new, branched);
     input [`WTG_OP_BIT - 1:0] op;
     input [31:0] off32;
@@ -71,6 +85,7 @@ module CmbWTG(op, off32, imm26, data_x, data_y, pc_4, pc_new, branched);
 endmodule
 
 // Brief: Arithmetic Logic Unit, combinatorial
+// Author: KailinLi
 module CmbALU(op, data_x, data_y, shamt, data_res);
     input [`ALU_OP_BIT - 1:0] op;
     input [31:0] data_x;
@@ -80,6 +95,7 @@ module CmbALU(op, data_x, data_y, shamt, data_res);
 endmodule
 
 // Brief: Data Memory, combinatorial
+// Author: cuishaobo
 module SynDataMem(clk, rst_n, en, op, w_en, addr_dbg, addr, data_dbg, data);
     input clk;
     input rst_n;
@@ -95,7 +111,8 @@ endmodule
 // Brief: Syscall Module, synchronized
 // Description: If $v0 is 34, display content of $a0 on the 7-segment display;
 //              Otherwise pause the execution of the program
-module SynSyscall(syscall_en, data_v0, data_a0, display, display_en, halt);
+// Author: KailinLi
+module CmbSyscall(syscall_en, data_v0, data_a0, display, display_en, halt);
     input syscall_en;       
     input [31:0] data_v0;
     input [31:0] data_a0;
@@ -105,6 +122,7 @@ module SynSyscall(syscall_en, data_v0, data_a0, display, display_en, halt);
 endmodule
 
 // Brief: Control Module, synchronized
+// Author: FluorineDog
 module CmbControl(opcode, rt, funct, op_wtg, op_alu, op_datamem, mux_regfile_req_w, mux_regfile_data_w, mux_alu_data_y);
     input [5:0] opcode;
     input [4:0] rt;
@@ -118,6 +136,7 @@ module CmbControl(opcode, rt, funct, op_wtg, op_alu, op_datamem, mux_regfile_req
 endmodule
 
 // Brief: CPU Top Module, synchronized
+// Author: EAirPeter
 module SynLajiIntelKnightsLanding(clk, rst_n, en, regfile_req_dbg, datamem_addr_dbg, regfile_data_dbg, datamem_data_dbg, display, display_en, halt);
     input clk;
     input rst_n;
@@ -130,3 +149,7 @@ module SynLajiIntelKnightsLanding(clk, rst_n, en, regfile_req_dbg, datamem_addr_
     output display_en;
     output halt;
 endmodule
+
+// Brief: Top Module, including I/O
+// Author: EAirPeter
+module TopLajiIntelKnightsLanding;
