f1a5c6979eb9a9a3a7078d1d6ba0c3d15e126caf
azure_crab <wuwanquan@126.com>
2018-03-13 15:00:17 +0800

    pre-debugged

--- CmbWTG.v
+++ CmbWTG.v
@@ -8,13 +8,13 @@ module CmbWTG(op, off32, imm26, data_x, data_y, pc_4, pc_new, branched);
     input [`WTG_OP_BIT - 1:0] op;
     input [31:0] off32;
     input [25:0] imm26;
-    input [31:0] data_x;
-    input [31:0] data_y;
+    input signed [31:0] data_x;
+    input signed [31:0] data_y;
     input [31:0] pc_4;
     output reg [31:0] pc_new;
     output reg branched;        // True on successful conditional branch
 
-        always @(*) begin
+    always @(*) begin
         case (op)
             `WTG_OP_J32 : begin
                 branched = 0;
@@ -26,28 +26,28 @@ module CmbWTG(op, off32, imm26, data_x, data_y, pc_4, pc_new, branched);
             end
             `WTG_OP_BEQ : begin
                 branched = (data_x == data_y);
-                pc_new = branched ? {off32[29:0], 2'b00} : pc_4;
+                pc_new = (branched ? {off32[29:0], 2'b00} : 0) + pc_4;
             end
             `WTG_OP_BNE : begin
                 branched = (data_x != data_y);
-                pc_new = branched ? {off32[29:0], 2'b00} : pc_4;
+                pc_new = (branched ? {off32[29:0], 2'b00} : 0) + pc_4;
             end
-            // `WTG_OP_BLEZ : begin
-            //     branched = (data_x <= 0);
-            //     pc_new = branched ? {off32[29:0], 2'b00} : pc_4;
-            // end
-            // `WTG_OP_BGTZ : begin
-            //     branched = (data_x > 0);
-            //     pc_new = branched ? {off32[29:0], 2'b00} : pc_4;              
-            // end
-            // `WTG_OP_BLTZ : begin
-            //     branched = (data_x < 0);
-            //     pc_new = branched ? {off32[29:0], 2'b00} : pc_4;              
-            // end
-            // `WTG_OP_BGEZ : begin
-            //     branched = (data_x >= 0);
-            //     pc_new = branched ? {off32[29:0], 2'b00} : pc_4;              
-            // end
+            `WTG_OP_BLEZ : begin
+                branched = (data_x <= 0);
+                pc_new = (branched ? {off32[29:0], 2'b00} : 0) + pc_4;
+            end
+            `WTG_OP_BGTZ : begin
+                branched = (data_x > 0);
+                pc_new = (branched ? {off32[29:0], 2'b00} : 0) + pc_4;
+            end
+            `WTG_OP_BLTZ : begin
+                branched = (data_x < 0);
+                pc_new = (branched ? {off32[29:0], 2'b00} : 0) + pc_4;
+            end
+            `WTG_OP_BGEZ : begin
+                branched = (data_x >= 0);
+                pc_new = (branched ? {off32[29:0], 2'b00} : 0) + pc_4;
+            end
             default : begin
                 branched = 0;
                 pc_new = pc_4;
--- CmbWTG_tb.v
+++ CmbWTG_tb.v
@@ -5,72 +5,74 @@
 // Description: Compute the value about to be updated into PC
 // Author: azure-crab
 module CmbWTG_tb();
-    reg [`WTG_OP_BIT - 1:0] ops[0:7];
-    reg [31:0] off32s[0:3];
-    reg [25:0] imm26s[0:1];
-    reg [31:0] data_xs[0:3];
-    reg [31:0] data_ys[0:3];
-    reg [31:0] pc_4s[0:3];
-    wire [31:0] pc_new;
-    wire branched;        // True on successful conditional branch
-    integer a = 0;
-    integer b = 0;
-    integer c = 0;
-    integer d = 0;
-    integer e = 0;
-    integer f = 0;
-
     reg [`WTG_OP_BIT - 1:0] op;
     reg [31:0] off32;
     reg [25:0] imm26;
     reg [31:0] data_x;
     reg [31:0] data_y;
     reg [31:0] pc_4;
+    wire [31:0] pc_new;
+    wire branched;        // True on successful conditional branch
+
     CmbWTG tb(op, off32, imm26, data_x, data_y, pc_4, pc_new, branched);
     initial begin
-        op[0] = `WTG_OP_J32;
-        op[1] = `WTG_OP_J26;
-        op[2] = `WTG_OP_BEQ;
-        op[3] = `WTG_OP_BNE;
-        op[4] = `WTG_OP_BLEZ;
-        op[5] = `WTG_OP_BGTZ;
-        op[6] = `WTG_OP_BLTZ;
-        op[7] = `WTG_OP_BGEZ;
-        off32s[0] = 'h12ff315a;
-        off32s[1] = 'h25a9c201;
-        off32s[2] = 'h549201bb;
-        off32s[3] = 'h5728910a;
-        imm26s[0] = 26'b10001001001101001010010010;
-        imm26s[1] = 26'b01110101110100110100101011;
-        data_xs[0] = -1;
-        data_xs[1] = 0;
-        data_xs[2] = 1;
-        data_xs[3] = 257;
-        data_ys[0] = -2;
-        data_ys[1] = 3;
-        data_ys[2] = 0;
-        data_ys[3] = 255;
-        pc_4s[0] = 124;
-        pc_4s[1] = 32768;
-        pc_4s[2] = ‭138413344‬;
-        pc_4s[3] = 1440;
-        for ( ; a < 8 ; a = a + 1) begin
-            for (; b < 4; b = b + 1) begin
-                for (; c < 2; c = c + 1) begin
-                    for (; d < 4; d = d + 1) begin
-                        for (; e < 4; e = e + 1) begin
-                            for (; f < 4; f = f + 1) begin
-                                #5 op <= ops[a];
-                                off32 <= off32s[b];
-                                imm26 <= imm26s[c];
-                                data_x <= data_xs[d];
-                                data_y <= data_ys[e];
-                                pc_4 <= pc_4s[f];
-                            end
-                        end
-                    end
-                end
-            end
-        end
+        pc_4 <= 'h1000;
+        off32 <= 'h1000; 
+        #50;
+        op <= `WTG_OP_J32;
+        data_x <= 'h80001000;
+
+        #50;
+        op <= `WTG_OP_J26;
+        imm26 <= 'h8086;
+
+        #50;
+        op <= `WTG_OP_BEQ;
+        data_x <= 1000;
+        data_y <= 1001;
+
+        #5;
+        data_x <= 10000;
+        data_y <= 10000;
+
+        #50;
+        op <= `WTG_OP_BNE;
+        data_x <= 'h1000;
+        data_y <= 'h1001;
+        #5; 
+        data_x <= 'h2000;
+        data_y <= 'h2000;
+
+        #50;
+        op = `WTG_OP_BLEZ;
+        data_x <= -1;
+        #5;
+        data_x <= 0;
+        #5;
+        data_x <= 1;
+
+        #50;
+        op = `WTG_OP_BGTZ;
+        data_x <= -1;
+        #5;
+        data_x <= 0;
+        #5;
+        data_x <= 1;
+
+        #50;
+        op = `WTG_OP_BLTZ;
+        data_x <= -1;        
+        #5;
+        data_x <= 0;
+        #5;
+        data_x <= 1;
+
+        #50;
+        op = `WTG_OP_BGEZ;
+        data_x <= -1;
+        #5;
+        data_x <= 0;
+        #5;
+        data_x <= 1;
     end
 endmodule
