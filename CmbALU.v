ce0b47927bab707d928af369d6fcfbd085befa4f
KailinLi <likailinhust@gmail.com>
2018-03-13 15:33:41 +0800

    update

--- CmbALU.v
+++ CmbALU.v
@@ -6,23 +6,58 @@ module CmbALU(op, data_x, data_y, shamt, data_res);
     input [31:0] data_x;
     input [31:0] data_y;
     input [4:0] shamt;      // Separate input for shift 
-    output [31:0] data_res;
+    output reg [31:0] data_res;
 
     localparam MASK = 5'b11111;
-    assign data_res = (op == `ALU_OP_AND)  ?  (data_x & data_y) :
-                      (op == `ALU_OP_OR)   ?  (data_x | data_y) :
-                      (op == `ALU_OP_XOR)  ?  (data_x ^ data_y) :
-                      (op == `ALU_OP_NOR)  ? ~(data_x | data_y) :
-                      (op == `ALU_OP_ADD)  ?  (data_x + data_y) :
-                      (op == `ALU_OP_SUB)  ?  (data_x - data_y) :
-                      (op == `ALU_OP_SLLV) ?  (data_x << (MASK & data_y)) :
-                      (op == `ALU_OP_SRAV) ?  $unsigned(($signed(data_x) >>> (MASK & data_y))) :
-                      (op == `ALU_OP_SRLV) ?  (data_x >> (MASK & data_y)) :
-                      (op == `ALU_OP_SLL)  ?  (data_x << shamt) :
-                      (op == `ALU_OP_SRA)  ?  $unsigned(($signed(data_x) >>> shamt)) :
-                      (op == `ALU_OP_SRL)  ?  (data_x >> shamt) :
-                      (op == `ALU_OP_SLT)  ?  ($signed(data_x) < $signed(data_y)) :
-                      (op == `ALU_OP_SLTU) ?  (data_x < data_y) :
-                      (op == `ALU_OP_LUI)  ?  (data_y << 'd16) : 0;
+
+    always @(*) begin
+      case (op)
+        `ALU_OP_AND:
+          data_res <= (data_x & data_y);
+        `ALU_OP_OR:
+          data_res <= (data_x | data_y);
+        `ALU_OP_XOR:
+          data_res <= (data_x ^ data_y);
+        `ALU_OP_NOR:
+          data_res <= ~(data_x | data_y);
+        `ALU_OP_ADD:
+          data_res <= (data_x + data_y);
+        `ALU_OP_SUB:
+          data_res <= (data_x - data_y);
+        `ALU_OP_SLLV:
+          data_res <= (data_x << (MASK & data_y));
+        `ALU_OP_SRAV:
+          data_res <= $unsigned(($signed(data_x) >>> (MASK & data_y)));
+        `ALU_OP_SRLV:
+          data_res <= (data_x >> (MASK & data_y));
+        `ALU_OP_SLL:
+          data_res <= (data_x << shamt);
+        `ALU_OP_SRA:
+          data_res <= $unsigned(($signed(data_x) >>> shamt));
+        `ALU_OP_SRL:
+          data_res <= (data_x >> shamt);
+        `ALU_OP_SLT:
+          data_res <= ($signed(data_x) < $signed(data_y));
+        `ALU_OP_SLTU:
+          data_res <= (data_x < data_y);
+        `ALU_OP_LUI:
+          data_res <= (data_y << 'd16);
+      endcase
+    end
+    // assign data_res = (op == `ALU_OP_AND)  ?  (data_x & data_y) :
+    //                   (op == `ALU_OP_OR)   ?  (data_x | data_y) :
+    //                   (op == `ALU_OP_XOR)  ?  (data_x ^ data_y) :
+    //                   (op == `ALU_OP_NOR)  ? ~(data_x | data_y) :
+    //                   (op == `ALU_OP_ADD)  ?  (data_x + data_y) :
+    //                   (op == `ALU_OP_SUB)  ?  (data_x - data_y) :
+    //                   (op == `ALU_OP_SLLV) ?  (data_x << (MASK & data_y)) :
+    //                   (op == `ALU_OP_SRAV) ?  $unsigned(($signed(data_x) >>> (MASK & data_y))) :
+    //                   (op == `ALU_OP_SRLV) ?  (data_x >> (MASK & data_y)) :
+    //                   (op == `ALU_OP_SLL)  ?  (data_x << shamt) :
+    //                   (op == `ALU_OP_SRA)  ?  $unsigned(($signed(data_x) >>> shamt)) :
+    //                   (op == `ALU_OP_SRL)  ?  (data_x >> shamt) :
+    //                   (op == `ALU_OP_SLT)  ?  ($signed(data_x) < $signed(data_y)) :
+    //                   (op == `ALU_OP_SLTU) ?  (data_x < data_y) :
+    //                   (op == `ALU_OP_LUI)  ?  (data_y << 'd16) : 0;
 
 endmodule
