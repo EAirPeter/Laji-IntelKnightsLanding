4a94d0f9f2c748643865e8cc355ece8adea46276
FluorineDog <fluorine@gmail.com>
2018-03-17 12:32:44 +0800

    minor_change

--- SynPS1.v
+++ SynPS1.v
@@ -3,21 +3,23 @@
 // Brief: Program Counter, sychronized
 // Description: Update program counter
 // Author: FluorineDog
-module SynPS1(clk, rst_n, en, inst_in, inst, pc_4_in, pc_4);
-  input clk;
-  input rst_n;  //negedge reset
-  input en;     //high enable normal
-  input [31:0] inst_in;
-  output reg [31:0] inst;
-  input [`IM_ADDR_BIT - 1: 0] pc_4_in;
-  output [`IM_ADDR_BIT - 1: 0] pc_4;
-
+module SynPS1(
+  input clk,
+  input rst_n,  //negedge reset
+  input en,     //high enable normal
+  input [31:0] inst_in,
+  output reg [31:0] inst,
+  input [`IM_ADDR_BIT - 1: 0] pc_4_in,
+  output reg [`IM_ADDR_BIT - 1: 0] pc_4
+);
 
   always @(posedge clk, negedge rst_n) begin
-    if (!rst_n)
-      inst <= 0:
-      pc_4 <= pc_4_in;
-    else
+    if (!rst_n) begin 
+      inst <= 0;
+      pc_4 <= 0;
+    end else begin
       inst <= inst_in;
+      pc_4 <= pc_4_in;
     end
+  end
 endmodule
